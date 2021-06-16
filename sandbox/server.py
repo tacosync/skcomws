edc3619900a93545a8007f7f22e90529ff68b3f4
Raymond Wu <virus.warnning@gmail.com>
2021-06-17 03:53:27 +0800

    可切換 async_mode 設計
    Mentions: #1

--- sandbox/server.py
+++ sandbox/server.py
@@ -1,17 +1,33 @@
 '''
 Socket.IO server for testing
 CLI:
-  python -m watchgod server.main
+  python -m watchgod server.main 1
 '''
 
 import socketio
 from sanic import Sanic
+from aiohttp import web
+import tornado.ioloop
+import tornado.web
 
+# TODO:
+# * asgi/Uvicorn
+# * asgi/Daphne
+
+ASYNC_MODE = 'tornado'
+PORT = 63047
+
+sio = socketio.AsyncServer(async_mode=ASYNC_MODE)
 count = 0
 
-app = Sanic(name='Just a simple service')
-sio = socketio.AsyncServer(async_mode='sanic')
-sio.attach(app)
+if ASYNC_MODE == 'aiohttp':
+    app = web.Application()
+if ASYNC_MODE == 'sanic':
+    app = Sanic(name='Just a simple service')
+if ASYNC_MODE == 'tornado':
+    app = tornado.web.Application([
+        (r"/socket.io/", socketio.get_tornado_handler(sio))
+    ])
 
 @sio.event
 async def connect(sid, environ, auth):
@@ -30,7 +46,18 @@ async def client_said(sid, data):
     await sio.emit('server_said', { 'message': message })
 
 def main():
-    app.run(port=63047)
+    print('=========================')
+    print('  async_mode = %s' % ASYNC_MODE)
+    print('=========================')
+    if ASYNC_MODE == 'aiohttp':
+        sio.attach(app)
+        web.run_app(app, port=PORT)
+    if ASYNC_MODE == 'tornado':
+        app.listen(PORT)
+        tornado.ioloop.IOLoop.current().start()
+    if ASYNC_MODE == 'sanic':
+        sio.attach(app)
+        app.run(port=PORT)
 
 if __name__ == '__main__':
     main()
